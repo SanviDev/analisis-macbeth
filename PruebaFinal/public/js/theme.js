b721a889d44d1a45563998546bcd89dfa4a15480
SanviDev <161404667+SanviDev@users.noreply.github.com>
2024-10-15 09:48:21 -0300

    Add files via upload

---
+++ PruebaFinal/public/js/theme.js
@@ -0,0 +1,6 @@
+const themeSwitch = document.getElementById('theme-switch');
+const body = document.querySelector('body');
+
+themeSwitch.addEventListener('click', () => {
+    body.classList.toggle('dark');
+}); 
