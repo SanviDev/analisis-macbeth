8283c28dd7ec4bf4c5ad6016b5332d8147e8285b
SanviDev <161404667+SanviDev@users.noreply.github.com>
2024-10-15 09:49:01 -0300

    Delete PruebaFinal directory

--- PruebaFinal/public/js/theme.js
+++
@@ -1,6 +0,0 @@
-const themeSwitch = document.getElementById('theme-switch');
-const body = document.querySelector('body');
-
-themeSwitch.addEventListener('click', () => {
-    body.classList.toggle('dark');
-}); 
